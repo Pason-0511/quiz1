89de731210e974006462c82628e20374dba90c84
Pason-0511 <78478212+Pason-0511@users.noreply.github.com>
2021-03-31 22:22:06 +0800

    Add files via upload

--- quiz1.js
+++ quiz1.js
@@ -1,11 +1,11 @@
- function change() {
- var header = document.getElementById("CGU123");
- header.innerHTML = "CSIE@CGU";
- var para = document.getElementById("CGU456");
- para.innerHTML = "«ç»ò¨º»ò´Î¡I¡I.";
- }
- function myFunction() {
- var btn = document.createElement("BUTTON");
- btn.innerHTML = "CLICK ME";
-document.body.appendChild(btn).addEventListener("click",change);
- }
+function change() {
+    $("#hid123").html("CSIE@CGU");
+    $("#pid123").html("æ€Žéº¼é‚£éº¼æ£’")
+}
+
+function new_button() {
+    var btn="<button id='change'>Change this document</button>";
+    $("body").append(btn);
+    $("#change").click(change);
+}
+$("#btn").click(new_button);
