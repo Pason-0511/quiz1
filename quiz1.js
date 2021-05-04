73dc1e764e8ab31533e26c09e632ec6e8bd99fc7
Pason-0511 <78478212+Pason-0511@users.noreply.github.com>
2021-05-04 21:35:40 +0800

    Update quiz1.js

--- quiz1.js
+++ quiz1.js
@@ -2,7 +2,6 @@ function change() {
     $("#hid123").html("CSIE@CGU");
     $("#pid123").html("怎麼那麼棒")
 }
-
 function new_button() {
     var btn="<button id='change'>Change this document</button>";
     $("body").append(btn);
